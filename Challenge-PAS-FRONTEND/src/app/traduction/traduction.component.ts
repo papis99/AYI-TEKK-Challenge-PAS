fa924b4894e7d7d92c9f6ed22a6653975a8107da
Babacar-Diago <diago1babacar@gmail.com>
2022-12-01 03:38:58 +0000

    version 1

---
+++ Challenge-PAS-FRONTEND/src/app/traduction/traduction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-traduction',
+  templateUrl: './traduction.component.html',
+  styleUrls: ['./traduction.component.scss']
+})
+export class TraductionComponent {
+
+}
